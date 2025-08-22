8c21437b45209443239122fce416150f1858b1a5
Ningziyun <49535971+Ningziyun@users.noreply.github.com>
2025-08-22 16:18:31 -0400

    initial sumbit

---
+++ transformer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -n 12
+#SBATCH --mem=192G
+#SBATCH -t 48:00:00
+# Request a GPU partition node and access to 2 GPU
+#SBATCH -p gpu --gres=gpu:2
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ning_yan@brown.edu
+
+source transformer_setup.sh
+python sample_jets.py --num_samples 10000 --model_dir models/test_6/
+#source transformer_run.sh
