51ff4e6db302d9d89f811155edd3b15e66bb456d
Ningziyun <49535971+Ningziyun@users.noreply.github.com>
2025-12-08 17:40:10 -0500

    Reset the range of lundselect
    Reset the range of lundselect to include log(kt) from -1 to 8

--- data_setup.sh
+++ data_setup.sh
@@ -2,4 +2,4 @@ source discretize.sh inputFiles/top_benchmark
 ./mergeTrees.py originalJets_qcd.root originalJets_qcd_*.root
 python addEnergy.py
 python lunddata.py --logMode
-python lund_select.py --in inputFiles/qcd_lund.root --out inputFiles/qcd_lund_cut.root --mode cut --xmin 0 --xmax 8 --ymin -1 --ymax 8 --mode swap
+python lund_select.py --in inputFiles/qcd_lund.root --out inputFiles/qcd_lund_cut.root --mode cut --xmin -1 --xmax 8 --ymin -1 --ymax 8 --mode swap
